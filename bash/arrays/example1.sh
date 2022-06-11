f4cb7c300736352c953efa6fd092b7cfde0f108c
Thales Faggiano <thalesfaggiano@gmail.com>
2022-06-11 13:02:28 -0300

    Adding simple error handling

--- bash/arrays/example1.sh
+++ bash/arrays/example1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Select and sum list based in first character
-list () {
+stock_list () {
   B=($(echo $1 | tr ' ,' '@ '))
   C=($(echo $2 | tr ' ,' '@ '))
+  [[ ${#B[@]} == "0" ]] && exit 0
   for y in ${C[@]}; do
     eval "int$y=0"
     for x in ${B[@]}; do
-      number=$(echo $x | cut -d'@' -f2)
+      number=$(echo $x | cut -d'@' -f2 | sed 's/^0*//')
       [[ $y == ${x::1} ]] && eval "int$y=$((int$y+$number))"
     done
     Stdout+=$(echo -n '('$y' : '$(eval echo \$int$y)')')
   done
   echo $Stdout | sed -e 's@)(@) - (@g'
 }
-list "$1" "$2"
+stock_list "$1" "$2"
